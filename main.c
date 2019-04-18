00baf23c415ef0e9d43f5bde7eef72351010d256
Ky Pollard <kypol06@gmail.com>
2019-04-18 23:43:07 +0000

    Update 1: only added part of a Menu

--- main.c
+++ main.c
@@ -1,8 +1,35 @@
 #include <stdio.h>
 
-int main() {
-    int a, b, c;
-    scanf("%d %d %d", &a, &b, &c);
-    printf("Found intergers %d %d %d\n", a, b, c);
-return 0;
+int main() 
+
+{
+   int c1;
+    int c2;
+    int c3;
+    int a;
+    int b;
+    int c;
+    int d;
+    printf("Choose a cihper: \n1 for Rotation Cihper\n2 for Subsition Cihper\n");
+    scanf("%d", &c1);
+
+        switch(c1){
+        case 1: c2=1;
+        break;
+        case 2: c2=2;
+        break;
+        default: printf("This is not an option\n");
+         }
+    printf("To chose Encryption please select 1, to chose Decryption please select 2\n");
+if(c2 == 1) {
+     c3=1;
+    }else if(c2 == 2) { 
+    c3=2;
 }
+
+ if(c3 == 1) {
+
+ } 
+ 
+    
+    return 0;
