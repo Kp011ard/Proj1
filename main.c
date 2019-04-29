2bee5f3eca0bda72130acdd78934ce66e81d6e60
Ky Pollard <kypol06@gmail.com>
2019-04-29 02:57:26 +0000

    Update 2.2: This is as much as I could achieve, the rotation encryption and decryption should work, and print it to a file and on the terminal.
    What i couldn't complete on time: Subsitution Chiper,  decryption without Key.

--- main.c
+++ main.c
@@ -9,6 +9,9 @@ char message[100], ch;
 scanf("%d", &a);
 scanf("%s", message);
 scanf("%d", &key);
+   FILE *fo;
+   fo= fopen("Endgame.txt","w");
+
 //From lines 6 to 13 are the code for the menu
 switch(a) {
     case 1: printf("Rotation Encryption selected\n"); b=1; break;
@@ -17,20 +20,44 @@ switch(a) {
     case 4: printf("Subsitution Decryption selected\n");b=4; break;
     default: printf("Program quiting"); exit (0);
 }
-if(b=1);{ 
+if(b==1){ //Lines 23 to 38 is the code for the rotation Encryption
  for(i = 0; message[i] != '\0'; ++i){
-		ch = message[i];
+		ch = message[i];//this makes the message into seperate intergers
 		if(ch>='A' && ch<='Z'){
-		ch=ch+key;
+		ch=ch+key;//this is the math behind the encryption
 		
 		}
-if(ch>='a' && ch<='z') {
+if(ch>='a' && ch<='z') {//lines 30 to 33 accounts to make all leters
+//captials
     ch = ch-32;
     ch=ch+key;
 }
-message[i] = ch;
+message[i] = ch; //this reverts the intergers to characters
 }
-
- }printf("%s", message);
- return 0;
+printf("%s", message);//This prints out the Encrypted message
+fprintf(fo, "%s", message);//This SHOULD print the message to a file.
+   fclose(fo);
+return 0;
  }
+ if(b==2) {//Lines 40 to 58 is code for rotation decryption
+     for(i = 0; message[i] != '\0'; ++i){
+		ch = message[i];//this makes the message into seperate intergers
+		if(ch>='A' && ch<='Z'){
+		ch=ch-key;//this is the math behind the encryption
+		
+		}
+if(ch>='a' && ch<='z') {//lines 47 to 50 accounts to make all leters
+    ch = ch-32;
+    ch=ch-key;
+}
+message[i] = ch;//this reverts the intergers to characters
+}
+printf("%s", message);//prints the message
+fprintf(fo, "%s", message);//This SHOULD print the message to a file.
+   fclose(fo);
+
+return 0;
+ }
+ }
+ 
+ 
